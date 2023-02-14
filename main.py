75508145380602ee143e002cbce1f8c8fa88b5f3
Cesar <mail.mansilla@gmail.com>
2023-02-14 20:38:11 -0300

    Muevo lógica a notebook

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,10 +17,6 @@ app.add_middleware(
 app.include_router(router)
 
 if __name__ == "__main__":
-  def init_webhooks(base_url):
-    # Update inbound traffic via APIs to use the public-facing ngrok URL
-    pass
-
   if USE_NGROK:
     # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
     from pyngrok import ngrok
@@ -35,13 +30,7 @@ if __name__ == "__main__":
     # Open a ngrok tunnel to the dev server
     public_url = ngrok.connect(port).public_url
     print("ngrok tunnel \"{}\" -> \"{}:{}\"".format(public_url, BASE_URL, port))
-
-    # Update any base URLs or webhooks to use the public ngrok URL
-    BASE_URL = public_url
-    init_webhooks(public_url)
   
-  #public_url = ngrok.connect(port=5000)
-  #print(f"La URL pública de tu aplicación es: {public_url}")
   config = uvicorn.Config("main:app", port=port, log_level="info", reload=True)
   server = uvicorn.Server(config)
   server.run()
